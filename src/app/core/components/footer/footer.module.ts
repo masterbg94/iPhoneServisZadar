1f274db90bfa2c1ec4342a72749edaccf5a2e2a9
Nemanja Milinkovic <nmilinkovic46@gmail.com>
2020-07-04 23:21:11 +0200

    * footer links

--- src/app/core/components/footer/footer.module.ts
+++ src/app/core/components/footer/footer.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 
 import {FooterComponent} from './footer.component';
 import {WebsiteTermsModule} from '../website-terms/website-terms.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    WebsiteTermsModule
+    WebsiteTermsModule,
+    RouterModule
   ],
   exports: [FooterComponent],
   declarations: [FooterComponent]
