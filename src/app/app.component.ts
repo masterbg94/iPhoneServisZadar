87870d2e2d011cbddd5905bcbc4cde306947588b
Nemanja Milinkovic <nmilinkovic46@gmail.com>
2020-11-01 21:47:23 +0100

    Initialized translate example * must be a lower version in npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, HostListener, OnInit} from '@angular/core';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map, share} from 'rxjs/operators';
+import {TranslateService} from '@ngx-translate/core';
 
 
 @Component({
@@ -10,8 +11,13 @@ import {map, share} from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(private breakpointObserver: BreakpointObserver, public translate: TranslateService) {
     this.innerWidth = window.innerWidth;
+
+    translate.addLangs(['en', 'hr']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|hr/) ? browserLang : 'en');
   }
 
   title = 'iPhone Servis Zadar pocetna';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,17 @@ import {AppComponent} from './app.component';
 import {ToolbarModule} from './core/components/toolbar/toolbar.module';
 import {AppRoutingModule} from './app.routing.module';
 import {FooterModule} from './core/components/footer/footer.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CustomMaterialModule} from './custom.material.module';
-import {HttpClientModule} from '@angular/common/http';
+
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -22,7 +30,14 @@ import {HttpClientModule} from '@angular/common/http';
     FooterModule,
     BrowserAnimationsModule,
     CustomMaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
